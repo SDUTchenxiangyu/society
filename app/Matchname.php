2ff450aef4eb3342c72583a69010770223458d5c
17864309139 <17864309139@163.com>
2017-10-26 17:27:37 +0800

    finish match joinin

---
+++ app/Matchname.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Matchname extends Model
+{
+    protected $table = 'matchname';
+    public $timestamps = false;
+}
---
+++ app/Usermatch.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Usermatch extends Model
+{
+    protected $table = 'usermatch';
+    public $timestamps = false;
+}
